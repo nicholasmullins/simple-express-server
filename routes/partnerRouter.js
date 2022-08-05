82507301257f80c393cc0aca7e102c2eeb635818
Nicholas Mullins <nicholasmullins@gmail.com>
2022-08-05 19:05:51 +0200

    set up simple server and made three routes for partners, campsites, and promotions.

---
+++ routes/partnerRouter.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const partnerRouter = express.Router()
+
+partnerRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Context-Type', 'text-plain');
+    next()
+})
+.get((req, res) => {
+    res.send('Will send all of the partners to you')
+})
+.post((req, res) => {
+    res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+})
+.delete((req, res) => {
+    res.end('Deleting all partners');
+});
+
+partnerRouter.route('/:partnerId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Context-Type', 'text-plain');
+    next()
+})
+.get((req, res) => {
+    res.send('Will send this partner to you')
+})
+.post((req, res) => {
+    res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+})
+.delete((req, res) => {
+    res.end('Deleting all partners');
+});
+module.exports = partnerRouter;
